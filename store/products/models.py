0c625e37377eb82172a3110f1e1d280b332cb067
Julia Matviychuk <juliamatviychuk@gmail.com>
2025-01-15 17:10:49 +0200

    configure Django ORM

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name_category = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True)
+
+class Product(models.Model):
+    name = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=3)
+    quantity = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    img = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True)
+
