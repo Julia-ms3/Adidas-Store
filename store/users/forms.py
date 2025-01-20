2f0f1cb58134cb90a35840f764176d2044b8c116
Julia Matviychuk <juliamatviychuk@gmail.com>
2025-01-20 10:32:47 +0200

    Create registration form

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,18 +1,51 @@
 from django import forms
 from users.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control py-4',
-        'placeholder':'enter username'
+        'placeholder': 'enter username'
     }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4',
-        'placeholder':'enter password'
+        'placeholder': 'enter password'
     }))
 
     class Meta:
         model = User
-        fields = ['username', 'password', '']
+        fields = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter first name'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter last name'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter username'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter email'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter password'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'enter confirm password'
+    }))
+
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email','password1', 'password2']
