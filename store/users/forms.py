765b4566f193fc2ec26c7a04c3aa07f712fd51dc
Julia Matviychuk <juliamatviychuk@gmail.com>
2025-01-20 09:33:52 +0200

    Create custom login template

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,7 +1,18 @@
+from django import forms
 from users.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder':'enter username'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder':'enter password'
+    }))
+
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', '']
