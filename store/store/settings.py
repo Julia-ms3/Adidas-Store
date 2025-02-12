57d23ed2c85c17e8f586cc07646c2420c2345575
Julia Matviychuk <Matviychukjulia03@gmail.com>
2025-02-12 14:27:59 +0200

    Replaced SQLite with PostgreSQL

--- store/store/settings.py
+++ store/store/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "adidas_store",
+        "USER": os.getenv('USER_DB'),
+        "PASSWORD": os.getenv('PASSWORD_DB'),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
